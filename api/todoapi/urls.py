87d6917dcd736808fd36c522f83859c55e91b28d
zerobits01 <zerobits0101@gmail.com>
2022-05-11 12:23:33 +0430

    adding authorization on get request only list

--- api/todoapi/urls.py
+++ api/todoapi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from todoapi.views import GoalAPI
+from todoapi.views import GoalAPI, DoneGoalAPI
 
 goal_router = DefaultRouter()
-goal_router.register(r'tasks', GoalAPI, basename="goal")
+goal_router.register(r'goals', GoalAPI, basename="goal")
+goal_router.register(r'done-goals', DoneGoalAPI, basename="done-goal")
 
 urlpatterns = [
     path('', include(goal_router.urls)),
--- api/todoapi/views.py
+++ api/todoapi/views.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from todoapi.models import Goal
 from todoapi.serializers import GoalSerializer
@@ -10,6 +11,24 @@ class GoalAPI(ModelViewSet):
     
     queryset = Goal.objects.all()
     serializer_class = GoalSerializer
-    http_method_names = ['post', 'get', 'delete', 'put']
+    http_method_names = ['post', 'get', 'delete', 'patch']
+    permission_classes = [permissions.IsAuthenticated, ]
+
+    # it doesnt have authorization on create and patch
+
+    def list(self, request):
+        data = list(self.queryset.filter(done=False, owner=request.user).values())
+        print(20*"#", data)
+        return Response(data)
+
+class DoneGoalAPI(ModelViewSet):
+    
+    queryset = Goal.objects.all()
+    serializer_class = GoalSerializer
+    http_method_names = ['get']
     permission_classes = [permissions.IsAuthenticated, ]
     
+    def list(self, request):
+        data = list(self.queryset.filter(done=True, owner=request.user).values())
+        print(20*"#", data)
+        return Response(data)
